1e2eaca12748cab897f4a29f0e069fd37ae3af11
ghbsunny <951408320@qq.com>
2018-12-21 00:38:36 +0800

    base_script

---
+++ base_scirpt/9*9.py
@@ -0,0 +1,22 @@
+print("first method")
+for i in range(1,10):
+    s = " "
+    for j in range (1,10):
+        if j < i+1:
+            s += str(j) + "*" + str(i) + "=" + str(i*j) + ' '
+    print(s)
+
+
+print("second method")
+for i in range(1,10):
+    s = " "
+    for j in range (1,i+1):
+            s += str(j) + "*" + str(i) + "=" + str(i*j) + ' '
+    print(s)
+
+
+print("third method")
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(str(j) + '*' + str(i) + '=' + str(i*j),end = '\t')
+    print()
