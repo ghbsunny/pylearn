936c7c957fa521b8b1aa1c2437e5e7e366490fc6
ghbsunny <951408320@qq.com>
2018-12-23 12:05:16 +0800

    base_script

--- base_scirpt/9*9.py
+++ base_scirpt/9*9.py
@@ -26,3 +26,20 @@ for i in range(1,10):
     for j in range (1,i+1):
         print('{}*{}={}\t'.format(j,i,j*i),end="")
     print()
+
+print("different way to show 9*9,methon 1:")
+for i in range(1,10):
+    for j in range (i,10):
+        print('{}*{}={}\t'.format(i,j,j*i),end="")
+    print()
+
+
+print("different way to show 9*9,methon 2:")
+for i in range(1,10):
+    s=' '*7*(i-1)
+    for j in range (i,10):
+        if i*j<10:
+            s += str(i) + '*' + str(j) + '=' + str(i*j)+ "  "
+        else:
+             s += str(i) + '*' + str(j) + '=' + str(i*j)+ " "
+    print(s)
